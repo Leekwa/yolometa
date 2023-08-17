afa136f843a7de74873541a76a60bab852a8aa04
Leekwa <fredkdkdkd@naver.com>
2023-08-17 10:21:58 +0900

    수정된 내용

--- train.py
+++ train.py
@@ -11,3 +11,5 @@ print(ultralytics.checks())
 if __name__ == '__main__':
     model = YOLO('yolov8n.pt')
     model.train(data='C:/Users/user/PycharmProjects/YOLO08/Fish-44/data.yaml', imgsz=416, batch=1, epochs=30, device=0)
+
+#수정

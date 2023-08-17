acd97a068a93c8bcc6e1368b36f78754f159970b
fred <fredkdkdkd@gmail.com>
2023-08-17 09:41:02 +0900

    first commit

---
+++ train.py
@@ -0,0 +1,13 @@
+import torch
+import ultralytics
+from ultralytics import YOLO
+
+print(torch.cuda.is_available())
+print('==================================')
+print(ultralytics.checks())
+
+
+
+if __name__ == '__main__':
+    model = YOLO('yolov8n.pt')
+    model.train(data='C:/Users/user/PycharmProjects/YOLO08/Fish-44/data.yaml', imgsz=416, batch=1, epochs=30, device=0)
